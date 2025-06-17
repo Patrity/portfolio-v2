3d30f77e76d31d57c509874cd8d9a9ee2da29127
Tony Costanzo <tonycos92@gmail.com>
2025-06-17 10:24:58 -0500

    feat: add about and contact WIP pages

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <UPageHero title="About This Project">
+    <template #description>
+      <p>
+        This project is built using 
+        <ULink raw to="https://nuxt.com" target="_blank" inactive-class="text-primary font-semibold">NuxtJS v4</ULink>, 
+        <ULink raw to="https://ui.nuxt.com" target="_blank" inactive-class="text-primary font-semibold">Nuxt UI Pro v3</ULink> and 
+        <ULink raw to="https://content.nuxt.com" target="_blank" inactive-class="text-primary font-semibold">Nuxt Content v3</ULink>. 
+      </p>
+      <p>
+        It deploys to 
+        <ULink raw to="https://www.cloudflare.com/products/pages/" target="_blank" inactive-class="text-primary font-semibold">Cloudflare Pages</ULink> 
+        utilizing 
+        <ULink raw to="https://nuxthub.com" target="_blank" inactive-class="text-primary font-semibold">NuxtHub</ULink>.
+      </p>
+      <p>
+        <ULink raw to="https://github.com/patrity/portfolio-v2" target="_blank" inactive-class="text-primary font-semibold">GitHub Repository</ULink>
+      </p>
+    </template>
+  </UPageHero>
 </template>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <UPage>
+    <UContainer class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-4xl font-bold mb-4">Contact Page</h1>
+      <p class="text-lg text-gray-600">This page is under construction. Please check back later!</p>
+    </UContainer>
+  </UPage>
 </template>
