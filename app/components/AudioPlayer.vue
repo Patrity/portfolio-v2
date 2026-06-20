c09bd71678f2a9cbcda5141693ae70aa3814e615
Tony Costanzo <tonycos92@gmail.com>
2026-06-20 09:09:46 -0500

    feat(narrate): AudioPlayer MDC component for post embeds

---
+++ app/components/AudioPlayer.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+defineProps<{ src: string }>()
+</script>
+
+<template>
+  <div class="my-4">
+    <slot />
+    <audio :src="src" controls preload="metadata" class="w-full" />
+  </div>
+</template>
